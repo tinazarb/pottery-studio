73ec2c298259936ce44b2bb0c4a90da9a45e00b7
Liying <hlyibic@gmail.com>
2022-06-14 12:45:17 -0400

    test

--- client/store/index.js
+++ client/store/index.js
@@ -3,7 +3,7 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import auth from './auth'
-
+// store
 const reducer = combineReducers({ auth })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
