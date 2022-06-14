99e0b4318c17808784dd9dd74f9e6e28a9459891
Liying <hlyibic@gmail.com>
2022-06-14 15:13:34 -0400

    complete API route and Redux store for all products

--- client/store/index.js
+++ client/store/index.js
@@ -2,12 +2,15 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
+import products from './products'
 import auth from './auth'
-// store
-const reducer = combineReducers({ auth })
+
+const reducer = combineReducers({ products, auth })
+
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
+
 const store = createStore(reducer, middleware)
 
 export default store
