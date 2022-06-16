cdfc4a7e030a482bfd219906e64887cba5b1c930
Lydia Kwa <lydiakwa@gmail.com>
2022-06-16 13:31:18 -0400

    updated model methods

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,89 +1,148 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const axios = require('axios');
 
 const SALT_ROUNDS = 5;
 
+const { STRING, VIRTUAL } = Sequelize;
+
 const User = db.define('user', {
   firstName: {
-    type: Sequelize.STRING,
+    type: STRING,
     allowNull: false,
   },
   lastName: {
-    type: Sequelize.STRING,
+    type: STRING,
     allowNull: false,
   },
   email: {
-    type: Sequelize.STRING,
+    type: STRING,
     allowNull: false,
     unique: true,
   },
   password: {
-    type: Sequelize.STRING,
+    type: STRING,
     allowNull: false,
   },
   address: {
-    type: Sequelize.STRING,
+    type: STRING,
   },
   isAdmin: {
-    type: Sequelize.BOOLEAN
-  }
+    type: BOOLEAN,
+  },
+  token: {
+    type: VIRTUAL,
+    get() {
+      const token = jwt.sign({ userId: this.id }, process.env.JWT);
+      return token;
+    },
+  },
 });
 
-module.exports = User;
-
-/**
- * instanceMethods
- */
-User.prototype.correctPassword = function(candidatePwd) {
-  //we need to compare the plain version to an encrypted version of the password
-  return bcrypt.compare(candidatePwd, this.password);
-}
-
-User.prototype.generateToken = function() {
-  return jwt.sign({id: this.id}, process.env.JWT)
-}
-
-/**
- * classMethods
- */
-User.authenticate = async function({ email, password }){
-    const user = await this.findOne({where: { email }})
-    if (!user || !(await user.correctPassword(password))) {
-      const error = Error('Incorrect email/password');
-      error.status = 401;
-      throw error;
+//searches by token and returns the whole user object -- class method
+User.byToken = async (token) => {
+  try {
+    const parsedToken = jwt.verify(token, process.env.JWT);
+    if (parsedToken) {
+      const user = await User.findByPk(parsedToken.userId);
+      return user;
     }
-    return user.generateToken();
+  } catch (err) {
+    const error = Error('bad credentials');
+    error.status = 401;
+    throw err;
+  }
 };
 
-User.findByToken = async function(token) {
-  try {
-    const {id} = await jwt.verify(token, process.env.JWT)
-    const user = User.findByPk(id)
-    if (!user) {
-      throw 'nooo'
+//checks if the user exists via email (should be unique) and returns the whole
+//user object back
+User.authenticate = async ({ email, password }) => {
+  const user = await User.findOne({
+    where: {
+      email,
+    },
+  });
+  if (user) {
+    const isValid = await bcrypt.compare(password, user.password);
+    if (isValid) {
+      return user;
     }
-    return user
-  } catch (ex) {
-    const error = Error('bad token')
-    error.status = 401
-    throw error
   }
-}
+  const error = Error('bad credentials');
+  error.status(401);
+  throw error;
+};
 
-/**
- * hooks
- */
-const hashPassword = async(user) => {
-  //in case the password has been changed, we want to encrypt it with bcrypt
-  if (user.changed('password')) {
-    user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
-  }
-}
+//hook - before user is created, password is hashed
+User.beforeCreate(async (user) => {
+  const SALT_COUNT = 5;
+  const hashedPw = await bcrypt.hash(user.password, SALT_COUNT);
+  user.password = hashedPw;
+});
 
-User.beforeCreate(hashPassword)
-User.beforeUpdate(hashPassword)
-User.beforeBulkCreate(users => Promise.all(users.map(hashPassword)))
+//instance method -- returns a json object
+User.prototype.toJSON = function () {
+  return {
+    token: this.token,
+    email: this.email,
+    profilePicture: this.profilePicture,
+  };
+};
+
+//-- old code
+// /**
+//  * instanceMethods
+//  */
+// User.prototype.correctPassword = function (candidatePwd) {
+//   //we need to compare the plain version to an encrypted version of the password
+//   return bcrypt.compare(candidatePwd, this.password);
+// };
+
+// User.prototype.generateToken = function () {
+//   return jwt.sign({ id: this.id }, process.env.JWT);
+// };
+
+// /**
+//  * classMethods
+//  */
+// User.authenticate = async function ({ email, password }) {
+//   const user = await this.findOne({ where: { email } });
+//   if (!user || !(await user.correctPassword(password))) {
+//     const error = Error('Incorrect email/password');
+//     error.status = 401;
+//     throw error;
+//   }
+//   return user.generateToken();
+// };
+
+// User.findByToken = async function (token) {
+//   try {
+//     const { id } = await jwt.verify(token, process.env.JWT);
+//     const user = User.findByPk(id);
+//     if (!user) {
+//       throw 'nooo';
+//     }
+//     return user;
+//   } catch (ex) {
+//     const error = Error('bad token');
+//     error.status = 401;
+//     throw error;
+//   }
+// };
+
+// /**
+//  * hooks
+//  */
+// const hashPassword = async (user) => {
+//   //in case the password has been changed, we want to encrypt it with bcrypt
+//   if (user.changed('password')) {
+//     user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
+//   }
+// };
+
+// User.beforeCreate(hashPassword);
+// User.beforeUpdate(hashPassword);
+// User.beforeBulkCreate((users) => Promise.all(users.map(hashPassword)));
+
+module.exports = User;
